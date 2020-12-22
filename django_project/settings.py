069ec88272d25e0cd8bb2afccdb87891235a6df9
PravegSth <pravegnsth@gmail.com>
2020-12-22 16:16:50 -0500

    Added Security

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x@r%$w)fyd@jv#-ad$iqj@s9og#e0#33vz)0j^q70^!p!ylk2p'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
